b6cd552e4a369d7d1f147d593e51cf8aec7c5c1c
udondan <deemes79@gmail.com>
2014-10-05 06:49:03 +0200

    Compatibility fixes with meteor >0.9.0

--- package.js
+++ package.js
@@ -1,10 +1,14 @@
 Package.describe({
   summary: "Contact form + mailer",
+  version: "0.4.0"
+  git: "https://github.com/udondan/meteor-contact.git",
 });
 
 Package.on_use(function(api, where){
-  api.use(["less", "templating", "bootstrap3-less"], "client");
-  api.use("meteor-nodemailer", "server");
+  api.versionsFrom("METEOR@0.9.0");
+  api.use(["less", "templating", "simison:bootstrap3-less"], "client");
+  api.use("mrt:meteor-nodemailer", "server");
+  api.use("mrt:trail", ["client", "server"], {weak: true});
   api.add_files(["lib/contact.html", "lib/contact.js", "lib/contact.less"], "client");
   api.add_files("lib/mailer.js", "server");
 });
